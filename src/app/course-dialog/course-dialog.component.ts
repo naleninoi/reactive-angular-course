c23d2b75742711c242ec4bcc1db5eb06f76a791c
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2024-05-10 15:56:04 +0300

    3-016

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -11,7 +11,10 @@ import { LoadingService } from '../services/loading.service';
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+    styleUrls: ['./course-dialog.component.css'],
+    providers: [
+      LoadingService
+    ]
 })
 export class CourseDialogComponent implements AfterViewInit {
 
@@ -43,7 +46,10 @@ export class CourseDialogComponent implements AfterViewInit {
 
     save() {
       const changes = this.form.value;
-      this.coursesService.saveCourse(this.course.id, changes)
+
+      const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes);
+
+      this.loadingService.showLoaderUntilCompleted(saveCourse$)
         .subscribe(updatedCourse => this.dialogRef.close(updatedCourse));
     }
 
