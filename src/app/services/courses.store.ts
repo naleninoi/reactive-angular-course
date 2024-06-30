d8806d841b46c51d50e326a6f7795280bbf2339a
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2024-06-30 18:06:40 +0300

    3-023

--- src/app/services/courses.store.ts
+++ src/app/services/courses.store.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Course, sortCoursesBySeqNo } from '../model/course';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { LoadingService } from './loading.service';
+import { MessagesService } from './messages.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesStore {
 
-  public courses$: Observable<Course[]>;
+  private subject = new BehaviorSubject<Course[]>([]);
+
+  public courses$: Observable<Course[]> = this.subject.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
+  ) {
+    this.loadAllCourses();
+  }
 
   public filterByCategory(category: string): Observable<Course[]> {
     return this.courses$.pipe(
@@ -18,4 +31,19 @@ export class CoursesStore {
     );
   }
 
+  private loadAllCourses() {
+    const loadCourses$ = this.http.get<Course[]>('/api/courses')
+      .pipe(
+        map(response => response['payload']),
+        catchError(err => {
+          const message = 'Could not load courses';
+          console.log(message, err);
+          this.messagesService.showErrors(message);
+          return throwError(err);
+        }),
+        tap(courses => this.subject.next(courses))
+      );
+
+    this.loadingService.showLoaderUntilCompleted(loadCourses$).subscribe();
+  }
 }
